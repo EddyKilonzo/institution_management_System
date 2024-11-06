53cb287ae374fc5bf234d17527196bc098549846
lil bendy <johnbrandews@gmail.com>
2024-11-06 11:55:50 +0300

    Add files via upload
    this a java swing based project that entails a full school management system that includes the admin section,teachers section and the student section

---
+++ Institution_management_System.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package institution_management_system;
+
+/**
+ *
+ * @author lil bendy
+ */
+public class Institution_management_System {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
